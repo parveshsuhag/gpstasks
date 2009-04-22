3be083d1c4bd4b2f7385bced88b206b95f8837a4
lincosmail <lincosmail@9cdbc420-268d-11de-a509-e7584d489398>
2009-04-22 17:38:59 +0000

    Доделал task_activity

--- GPSTasks/src/org/chudsaviet/GPSTasks/Test.java
+++ GPSTasks/src/org/chudsaviet/GPSTasks/Test.java
@@ -2,13 +2,13 @@ package org.chudsaviet.GPSTasks;
 
 import android.app.*;
 import android.os.Bundle;
-import android.widget.*;
+import com.bitsetters.android.*;
 
 public class Test extends Activity {
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.place_activity);
+        setContentView(R.layout.task_activity);
     }
 }
