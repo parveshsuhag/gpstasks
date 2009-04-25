1997e4d8de49e01061bbb3aa1317701e2a205e9b
lincosmail <lincosmail@9cdbc420-268d-11de-a509-e7584d489398>
2009-04-25 05:48:09 +0000

    Создал *.java - каркас Actvity'ей

---
+++ GPSTasks/src/org/chudsaviet/GPSTasks/PlaceActivity.java
@@ -0,0 +1,13 @@
+package org.chudsaviet.GPSTasks;
+
+import android.app.*;
+import android.os.Bundle;
+
+public class PlaceActivity extends Activity {
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.place_activity);
+    }
+}
