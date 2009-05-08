8ad7855a762cb3a0039f420fe7feaf97ab974ef9
lincosmail <lincosmail@9cdbc420-268d-11de-a509-e7584d489398>
2009-05-08 18:48:06 +0000

    Further implementation of TaskActivity

--- trunk/GPSTasks/src/org/chudsaviet/GPSTasks/TaskActivity.java
+++ trunk/GPSTasks/src/org/chudsaviet/GPSTasks/TaskActivity.java
@@ -14,15 +14,119 @@ import android.database.sqlite.*;
 public class TaskActivity extends Activity {
 	
 	static int MINUTES_SCROLL_INTERVAL=5;
+	static int MINUTES_DEFAULT=30;
 	
+	enum State {ViewingEditing,NewTask};
 	
 	SQLiteDatabase db;
 	Cursor cur;
 	int desired_time=30;//in minutes
+	State state;
+	int idtask;
 	
+	private class InsertNewTaskListener implements OnClickListener
+	{
+		@Override
+		public void onClick(View v) {
+					Log.d(getString(R.string.app_name),"saving new task");
+					
+					
+					setMode(State.ViewingEditing);
+					
+		}
+		
+	}
+	
+	private class DeleteTaskListener implements OnClickListener
+	{
+		@Override
+		public void onClick(View v) {
+					Log.d(getString(R.string.app_name),"deleting task");
+					
+					String query="DELETE FROM task WHERE idtask="+idtask;
+					
+					try{				
+						db.execSQL(query);
+						requery();
+					}
+					catch(Exception e)
+				        {
+				        	Log.e(getString(R.string.app_name), e.toString());
+				        }
+		}
+		
+	}
+	
+	private void requery()
+	{
+		cur.close();
+		cur=db.query("task", null, null, null, null, null, "idtask");
+		fillFieldsFromCursor();
+	}
+	
+	private void setMode(State state)
+	{
+		this.state=state;
+		
+		switch (state) {
+		case NewTask:
+			cur.close();
+			
+			Button b=(Button)findViewById(R.id.task_new_save);
+			b.setText(this.getString(R.string.save));
+			b.setOnClickListener(new InsertNewTaskListener());
+			
+			
+			ImageButton ib=(ImageButton)findViewById(R.id.task_button_previuos);
+			ib.setClickable(false);
+			
+			ib=(ImageButton)findViewById(R.id.task_button_next);
+			ib.setClickable(false);
+			
+			EditText name=(EditText)findViewById(R.id.task_name_field);
+			name.setText(this.getString(R.string.enter_name_of_the_task));
+			
+			EditText comments=(EditText)findViewById(R.id.task_comments_field);
+			comments.setText("");
+			
+			EditText dtime=(EditText)findViewById(R.id.task_desired_time_field);
+			dtime.setText(Integer.toString(MINUTES_DEFAULT));
+			
+			break;
+		case ViewingEditing:
+			
+			cur=db.query("task", null, null, null, null, null, "idtask");
+			cur.moveToFirst();
+			this.fillFieldsFromCursor();
+			
+			ImageButton ib1=(ImageButton)findViewById(R.id.task_button_previuos);
+			ib1.setClickable(true);
+			
+			ib1=(ImageButton)findViewById(R.id.task_button_next);
+			ib1.setClickable(true);
+			
+			
+			Button b1=(Button)findViewById(R.id.task_new_save);
+			b1.setText(this.getString(R.string.create_new_task));
+			b1.setOnClickListener(     new OnClickListener()
+										{
+								    		@Override
+								    		public void onClick(View v) {
+								    			setMode(State.NewTask);
+								    		}
+										}
+	        );
+			
+			b1=(Button)findViewById(R.id.task_delete);
+			b1.setOnClickListener(new DeleteTaskListener());
+		break;
+		}
+	}
 	
 	private void fillFieldsFromCursor()
 	{
+		idtask=cur.getInt(0);
+		
 		EditText name=(EditText)findViewById(R.id.task_name_field);
 		name.setText(cur.getString(1));
 		
@@ -46,9 +150,18 @@ public class TaskActivity extends Activity {
         	
         	
         	cur=db.query("task", null, null, null, null, null, "idtask");
-        	cur.moveToFirst();
-        	this.fillFieldsFromCursor();
-        	
+        	if(cur.getCount()!=0)
+        	{
+        		state=State.ViewingEditing;
+	        	cur.moveToFirst();
+	        	this.fillFieldsFromCursor();
+	        	
+	        	this.setMode(State.ViewingEditing);
+        	}
+        	else
+        	{
+        		this.setMode(State.NewTask);
+        	}
         }
         catch(Exception e)
         {
@@ -56,8 +169,8 @@ public class TaskActivity extends Activity {
         }
         
         
-        ImageButton b=(ImageButton)findViewById(R.id.task_button_previuos);
-		b.setOnClickListener(     new OnClickListener()
+        ImageButton ib=(ImageButton)findViewById(R.id.task_button_previuos);
+		ib.setOnClickListener(     new OnClickListener()
 									{
 							    		@Override
 							    		public void onClick(View v) {
@@ -69,8 +182,8 @@ public class TaskActivity extends Activity {
 									}
         					);
 		
-		b=(ImageButton)findViewById(R.id.task_button_next);
-		b.setOnClickListener(     new OnClickListener()
+		ib=(ImageButton)findViewById(R.id.task_button_next);
+		ib.setOnClickListener(     new OnClickListener()
 									{
 							    		@Override
 							    		public void onClick(View v) {
@@ -81,8 +194,8 @@ public class TaskActivity extends Activity {
 							    		}
 									}
         );
-		b=(ImageButton)findViewById(R.id.task_time_button_dec);
-		b.setOnClickListener(     new OnClickListener()
+		ib=(ImageButton)findViewById(R.id.task_time_button_dec);
+		ib.setOnClickListener(     new OnClickListener()
 									{
 							    		@Override
 							    		public void onClick(View v) {
@@ -95,8 +208,8 @@ public class TaskActivity extends Activity {
 									}
         );
 		
-		b=(ImageButton)findViewById(R.id.task_time_button_inc);
-		b.setOnClickListener(     new OnClickListener()
+		ib=(ImageButton)findViewById(R.id.task_time_button_inc);
+		ib.setOnClickListener(     new OnClickListener()
 									{
 							    		@Override
 							    		public void onClick(View v) {
@@ -107,5 +220,7 @@ public class TaskActivity extends Activity {
 							    		}
 									}
         );
+		
+		
 	}
 }
